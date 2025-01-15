a9c56e44c544a5b90979c7d3c73591afef5782fc
CJDAutorio <cjdautorioalt@gmail.com>
2025-01-15 16:53:15 -0500

    Enhance ThreeJSScene by adding ground and box models, and integrate Loader for suspense fallback

--- src/app/Components/ThreeJSScene/index.tsx
+++ src/app/Components/ThreeJSScene/index.tsx
@@ -6,20 +6,28 @@ import "./style.css";
 import { ComputerModel } from "./Components/computer";
 import { DeskModel } from "./Components/desk";
 import { GrassInstances } from "./Components/grass";
-import { Environment } from "@react-three/drei";
+import { GroundModel } from "./Components/ground";
+import { BoxInstances } from "./Components/cubes";
+import { Environment, OrbitControls } from "@react-three/drei";
+import Loader from "../Loader";
 
 export function ThreeJSScene() {
 	return (
-		<div className="w-screen h-screen absolute top-0 left-0 z-0 overflow-hidden">
+		<div className="w-screen h-screen absolute top-0 left-0 z-0 overflow-hidden bg-slate-300">
 			<h1>ThreeJSScene</h1>
-			<Canvas>
+			<Canvas
+				camera={{ fov: 10, near: 0.1, far: 1000, position: [0.5, 1, 18], rotation: [0, 0, 0] }}
+			>
 				<ambientLight intensity={0.1} />
-				<Suspense fallback={null}>
-                    <Environment preset="sunset" background />
+				<Suspense fallback={<Loader />}>
+					<Environment preset="sunset" background={false} />
 					<ComputerModel />
 					<DeskModel />
 					<GrassInstances />
+                    <GroundModel />
+                    <BoxInstances />
 				</Suspense>
+                {/* <OrbitControls /> */}
 			</Canvas>
 		</div>
 	);
