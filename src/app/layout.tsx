4a4ee1a21406ff2f381673a394129fa1f88e7211
CJDAutorio <cjdautorioalt@gmail.com>
2025-01-15 12:53:26 -0500

    Integrate ThreeJSScene component into RootLayout and adjust layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,37 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Landing from "./Views/Landing";
-import GrainyBorder from "./Components/GrainyBorder";
-import { Analytics } from "@vercel/analytics/react"
+import { ThreeJSScene } from "./Components/ThreeJSScene";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "CJ DAutorio",
-  description: "CJ DAutorio's personal website",
+	title: "CJ DAutorio",
+	description: "CJ DAutorio's personal website",
 };
 
 export default function RootLayout() {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Analytics />
-        <Landing className={'container mx-auto'} />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-50`}
+			>
+				<Analytics />
+				<ThreeJSScene />
+				<div className="z-10">
+					<Landing className={"container mx-auto"} />
+				</div>
+			</body>
+		</html>
+	);
 }
