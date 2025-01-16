1510eb48a1c479b7958535493384af9151fb143a
CJDAutorio <cjdautorioalt@gmail.com>
2025-01-16 00:53:53 -0500

    Add CameraController for smoother camera movement in ThreeJSScene

--- src/app/Components/ThreeJSScene/index.tsx
+++ src/app/Components/ThreeJSScene/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import { Camera, Canvas, useFrame } from "@react-three/fiber";
 import "./style.css";
 import { ComputerModel } from "./Components/computer";
 import { DeskModel } from "./Components/desk";
@@ -12,9 +12,9 @@ import { Environment } from "@react-three/drei";
 import Loader from "../Loader";
 
 export function ThreeJSScene() {
+
 	return (
 		<div className="w-screen h-screen absolute top-0 left-0 z-0 overflow-hidden bg-slate-300">
-			<h1>ThreeJSScene</h1>
 			<Canvas
 				camera={{
 					fov: 10,
@@ -23,32 +23,34 @@ export function ThreeJSScene() {
 					position: [0, 1, 18],
 					rotation: [0, 0, 0],
 				}}
-				onCreated={({ camera }) => {
-					let sway = 0;
-					const swaySpeed = 0.001;
-					const swayAmount = 0.05;
-
-					function animate() {
-						sway += swaySpeed;
-						camera.position.x = Math.cos(sway) * swayAmount;
-						camera.position.y = 1 + Math.sin(sway) * swayAmount;
-						requestAnimationFrame(animate);
-					}
-
-					animate();
-				}}
 			>
 				<ambientLight intensity={0.1} />
 				<Suspense fallback={<Loader />}>
 					<Environment preset="sunset" background={false} />
+					<CameraController />
 					<ComputerModel />
 					<DeskModel />
 					<GrassInstances />
 					<GroundModel />
 					<BoxInstances />
 				</Suspense>
-				{/* <OrbitControls /> */}
 			</Canvas>
 		</div>
 	);
+}
+
+function CameraController() {
+	const [targetX, setTargetX] = useState(0);
+	const [targetY, setTargetY] = useState(0);
+
+	useFrame((state) => {
+		const { x, y } = state.pointer;
+		setTargetX(Math.min(Math.max(x * 0.1, -0.5), 0.5));
+		setTargetY(0.5 + Math.min(Math.max(y * 0.1 + 0.5, 0), 1.5));
+
+		state.camera.position.x += (targetX - state.camera.position.x) * 0.01;
+		state.camera.position.y += (targetY - state.camera.position.y) * 0.01;
+	});
+
+	return null;
 }
