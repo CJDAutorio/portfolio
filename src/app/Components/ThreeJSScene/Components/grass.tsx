3bcf9c26f83bf60570a2fdcd18c401bfd4e12c8e
CJDAutorio <cjdautorioalt@gmail.com>
2025-01-16 00:53:59 -0500

    Refactor GrassInstances component for improved density and random scaling

--- src/app/Components/ThreeJSScene/Components/grass.tsx
+++ src/app/Components/ThreeJSScene/Components/grass.tsx
@@ -1,11 +1,7 @@
 import * as THREE from "three";
 import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { BufferAttribute, ShaderMaterial } from "three";
 import { GLTF } from "three-stdlib";
-import { extend, useFrame } from "@react-three/fiber";
-
-extend({ ShaderMaterial });
 
 type GLTFResult = GLTF & {
 	nodes: {
@@ -26,11 +22,9 @@ export function GrassInstances(
 ) {
 	const instancedMeshRef = useRef<THREE.InstancedMesh | null>(null);
 	const size = (props.size || 100) * 5;
-	const density = props.density || 0.1;
-	const offsetX = props.offsetX || density * size * -0.5;
-	// const offsetZ = props.offsetZ || density * size * -0.5;
+	const density = props.density || 0.25;
+	const offsetX = props.offsetX || density * Math.round(size / 4) * -0.5;
 	const offsetZ = props.offsetZ || -35;
-	const shaderMaterialRef = useRef<THREE.ShaderMaterial | null>(null);
 
 	const { nodes } = useGLTF("Assets/Models/grass.glb") as GLTFResult;
 
@@ -43,13 +37,15 @@ export function GrassInstances(
 		let currentZ = offsetZ;
 
 		for (let i = 0; i < size; i++) {
-			for (let j = 0; j < size; j++) {
+			for (let j = 0; j < Math.round(size / 2); j++) {
 				temp.position.set(
 					currentX + (Math.random() - 0.5) * 0.2,
 					0 - Math.random() * 0.3,
 					currentZ + (Math.random() - 0.5) * 0.2
 				);
 				temp.rotation.set(1.5707963, 0, 0);
+				const randomScale = Math.random() + 0.5;
+				temp.scale.set(randomScale, randomScale, randomScale);
 				temp.updateMatrix();
 				instancedMeshRef.current.setMatrixAt(currentIndex, temp.matrix);
 				currentIndex++;
@@ -61,13 +57,6 @@ export function GrassInstances(
 		instancedMeshRef.current.instanceMatrix.needsUpdate = true;
 	}, [size, density, offsetX, offsetZ]);
 
-	useFrame(({ clock }) => {
-		if (shaderMaterialRef.current) {
-			shaderMaterialRef.current.uniforms.time.value =
-				clock.getElapsedTime();
-		}
-	});
-
 	return (
 		<group {...props} dispose={null}>
 			<group name="Scene">
@@ -75,7 +64,7 @@ export function GrassInstances(
 					ref={instancedMeshRef}
 					args={[nodes.Grass.geometry, undefined, size * size]}
 				>
-					<meshBasicMaterial color='rgb(120, 132, 89)' attach='material'/>
+                    <meshBasicMaterial color='rgb(120, 132, 89)' attach='material'/>
 				</instancedMesh>
 			</group>
 		</group>
