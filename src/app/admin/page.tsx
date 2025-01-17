6a54bdc44357183c8c94b3e95699e95c0d839c8e
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-17 18:29:29 -0500

    feat: add form structure to Admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function Admin() {
 	return (
 		<div className="container mx-auto">
-            <h1>Admin Page</h1>
+			<h1>Admin Page</h1>
 			<div>
-				
+				<form>
+				</form>
 			</div>
 		</div>
 	);
