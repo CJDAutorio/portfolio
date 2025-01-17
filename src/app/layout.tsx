5fb10ac59c7b7e3909de2fdfbebb89a066a9ae64
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-17 17:55:22 -0500

    style: format layout.tsx for improved readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,34 +5,34 @@ import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "CJ DAutorio",
-  description: "CJ DAutorio's personal website",
+	title: "CJ DAutorio",
+	description: "CJ DAutorio's personal website",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-		<SpeedInsights />
-		<Analytics />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<SpeedInsights />
+				<Analytics />
+				{children}
+			</body>
+		</html>
+	);
 }
