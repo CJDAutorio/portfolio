50872b32f8eec7fb7ccb65ba6c4929c5fc336beb
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-17 18:13:46 -0500

    style: update Admin page layout for improved responsiveness

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Admin() {
 	return (
-		<div>
+		<div className="container mx-auto">
             <h1>Admin Page</h1>
+			<div>
+				
+			</div>
 		</div>
 	);
 }
