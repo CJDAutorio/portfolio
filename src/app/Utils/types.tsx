f1b56d9989330e39804a8ab388c87ba81cedb7b9
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-17 08:38:48 -0500

    chore: extend React's IntrinsicElements type with ThreeElements

--- src/app/Utils/types.tsx
+++ src/app/Utils/types.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { ThreeElements } from '@react-three/fiber';
 
 export interface Button {
 	name: string;
 	component: React.ReactNode;
+}
+
+declare module 'react' {
+  type IntrinsicElements = ThreeElements
 }
