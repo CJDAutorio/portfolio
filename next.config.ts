cae95f18415c62c056a99032835b2840a02ddfff
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-17 18:21:30 -0500

    feat: add remote image patterns for Vercel Blob storage

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['three'],
-  eslint: {
-      ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: true,
+	transpilePackages: ["three"],
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "my-blob-store.public.blob.vercel-storage.com",
+				port: "",
+			},
+		],
+	},
 };
 
 export default nextConfig;
