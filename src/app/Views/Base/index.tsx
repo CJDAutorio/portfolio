5dc96556c563a905927270fb8e34863fef17a55f
CJDAutorio <cjdautorioalt@gmail.com>
2025-01-17 02:28:07 -0500

    Enhance Base component to manage dynamic views with buttons and integrate AboutMe component

--- src/app/Views/Base/index.tsx
+++ src/app/Views/Base/index.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
-import Nav from '../Nav';
+"use client";
+
+import React from "react";
+import Nav from "../Nav";
+import AboutMe from "../AboutMe";
+import { Button } from "../../Utils/types";
 
 interface BaseComponentProps {
-    className?: string | undefined;
+	className?: string | undefined;
 }
 
 const Base: React.FC<BaseComponentProps> = ({ className }) => {
-    return (
-        <div className={`${className} relative w-full h-full`}>
-            <Nav />
-        </div>
-    );
+	const [view, setView] = React.useState<React.ReactNode>(null);
+
+	const buttons: Button[] = [
+		{
+			name: "About Me",
+            component: <AboutMe />,
+		},
+		{
+			name: "Experience",
+            component: <AboutMe />,
+		},
+		{
+			name: "Hobbies",
+            component: <AboutMe />,
+		},
+		{
+			name: "Contact Me",
+            component: <AboutMe />,
+		},
+	];
+
+	const handleViewChange = (viewIndex: number) => {
+        if (viewIndex === -1) {
+            setView(null);
+            console.log('removed view');
+            return;
+        }
+
+        setView(buttons[viewIndex].component);
+        console.log('set view to', buttons[viewIndex].name);
+    };
+
+	return (
+		<div className={`${className} relative w-full h-full`}>
+            <div className={`${view ? 'mb-12 h-0' : 'mb-0 h-full'}`}>{view}</div>
+			<Nav onViewChange={handleViewChange} buttons={buttons} />
+		</div>
+	);
 };
 
 export default Base;
