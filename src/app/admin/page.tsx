1635be6555abba6abe2e7404adb4db37d281cb1b
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-18 06:22:34 -0500

    feat: integrate FirebaseUI for enhanced authentication in Admin component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import React, { FormEvent, useEffect } from "react";
+import React, { useEffect } from "react";
 import { AboutMeForm } from "../Views/Admin/AboutMeForm/about-me-form.component";
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+import FirebaseAuth from "../Utils/FirebaseAuth";
 
 export default function Admin() {
 	const [view, setView] = React.useState<React.ReactNode>(null);
@@ -13,6 +17,33 @@ export default function Admin() {
 		setView(component);
 		console.log("set view to", component);
 	}
+
+	useEffect(() => {
+        const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(FirebaseAuth);
+
+        ui.start('#firebaseui-auth-container', {
+            callbacks: {
+                signInSuccessWithAuthResult: function(_authResult, _redirectUrl) {
+                    setIsSignedIn(true);
+					return false;
+                },
+                uiShown: function() {
+                    // This is what should happen when the form is full loaded. In this example, I hide the loader element.
+                    document.getElementById('loader')!.style.display = 'none';
+                }
+            },
+            // signInSuccessUrl: 'https://www.anyurl.com', // This is where should redirect if the sign in is successful.
+            signInOptions: [ // This array contains all the ways an user can authenticate in your application. For this example, is only by email.
+                {
+                    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                    requireDisplayName: true,
+                    disableSignUp: {
+                        status: true
+                    }
+                }
+            ],
+        });
+    }, []);
 
 	return (
 		<div className="container mx-auto mt-12 w-full">
