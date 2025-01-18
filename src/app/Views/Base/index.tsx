4e3d079e3431686d4f3f7c6db3cd3c5aef87e381
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-18 05:25:38 -0500

    feat: integrate Firebase for analytics in Base component

--- src/app/Views/Base/index.tsx
+++ src/app/Views/Base/index.tsx
@@ -4,8 +4,8 @@ import React from "react";
 import Nav from "../Nav";
 import AboutMe from "../AboutMe";
 import { Button } from "../../Utils/types";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 interface BaseComponentProps {
 	className?: string | undefined;
@@ -14,7 +14,18 @@ interface BaseComponentProps {
 const Base: React.FC<BaseComponentProps> = ({ className }) => {
 	const [view, setView] = React.useState<React.ReactNode>(null);
 
-	const dbClient = new DynamoDBClient({ region: "us-east-2" });
+	const firebaseConfig = {
+		apiKey: process.env.FIREBASE_API_KEY,
+		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+		appId: process.env.FIREBASE_APP_ID,
+		measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+	};
+	
+	const app = initializeApp(firebaseConfig);
+	const analytics = getAnalytics(app);
 
 	const buttons: Button[] = [
 		{
