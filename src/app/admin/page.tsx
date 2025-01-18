622e508010bd752f5c9bafe54746bbb0c2a7d7bb
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-18 06:08:18 -0500

    feat: refactor Admin component to remove FirebaseUI and streamline authentication handling

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,16 +2,6 @@
 
 import React, { FormEvent, useEffect } from "react";
 import { AboutMeForm } from "../Views/Admin/AboutMeForm/about-me-form.component";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-
-// Configure FirebaseUI.
-const uiConfig = {
-	signInFlow: "popup",
-	signInSuccessUrl: "/signedIn",
-	signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
-};
 
 export default function Admin() {
 	const [view, setView] = React.useState<React.ReactNode>(null);
@@ -19,41 +9,19 @@ export default function Admin() {
 
 	const buttonClasses = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
 
-	if (!firebase.apps.length) {
-		const firebaseConfig = {
-			apiKey: process.env.FIREBASE_API_KEY,
-			authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-			projectId: process.env.FIREBASE_PROJECT_ID,
-			storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-			messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-			appId: process.env.FIREBASE_APP_ID,
-			measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-		};
-		firebase.initializeApp(firebaseConfig);
-	}
-
 	function handleViewChange(component: React.ReactNode) {
 		setView(component);
 		console.log("set view to", component);
 	}
 
-	useEffect(() => {
-		const unregisterAuthObserver = firebase
-			.auth()
-			.onAuthStateChanged((user) => {
-				setIsSignedIn(!!user);
-			});
-		return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
-	}, []);
-
 	return (
 		<div className="container mx-auto mt-12 w-full">
 			<h1 className="text-4xl mb-16">Admin Page</h1>
-			{isSignedIn ? (
-				<StyledFirebaseAuth
-					uiConfig={uiConfig}
-					firebaseAuth={firebase.auth()}
-				/>
+			{!isSignedIn ? (
+				<div>
+					<p>Please sign in:</p>
+					<div id="firebaseui-auth-container"></div>
+				</div>
 			) : (
 				<div>
 					<div className="flex flex-row w-full justify-around gap-x-2">
