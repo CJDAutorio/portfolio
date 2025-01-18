914c94d2462e6162c0ae0b450a7efafac38c48d8
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-18 02:33:29 -0500

    feat: integrate AWS SDK DynamoDB client into admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,14 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import React from "react";
 
 export default function Admin() {
+	const dbClient = new DynamoDBClient({});
+	const docClient = DynamoDBDocumentClient.from(dbClient);
+	
 	return (
 		<div>
-            <h1>Admin Page</h1>
+			<h1>Admin Page</h1>
 		</div>
 	);
 }
