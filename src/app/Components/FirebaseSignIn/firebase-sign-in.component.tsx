7a32c083d2652f6378208c267887ccea9a1018fd
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-18 07:14:02 -0500

    feat: add FirebaseSignIn component for enhanced authentication flow

---
+++ src/app/Components/FirebaseSignIn/firebase-sign-in.component.tsx
@@ -0,0 +1,44 @@
+import FirebaseAuth from "@/app/Utils/FirebaseAuth";
+import firebase from "firebase/compat/app";
+import * as firebaseui from 'firebaseui'
+import { useEffect } from "react";
+import NoSSRComponent from "../NoSSR/no-s-s-r.component";
+
+interface FirebaseSignInProps {
+	setIsSignedIn: (isSignedIn: boolean) => void;
+}
+
+export function FirebaseSignIn({ setIsSignedIn }: FirebaseSignInProps) {
+	useEffect(() => {
+		const ui = new firebaseui.auth.AuthUI(FirebaseAuth);
+
+		const uiConfig = {
+			signInOptions: [
+				{
+					provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+					requireDisplayName: false,
+				},
+			],
+			disableSignUp: {
+				status: true,
+				errorMessage: "Bad dog.",
+			},
+			signInFlow: "popup",
+			callbacks: {
+				signInSuccessWithAuthResult: function () {
+					setIsSignedIn(true);
+					return false;
+				},
+			},
+		};
+
+		if (ui.isPendingRedirect()) {
+			ui.start("#firebaseui-auth-container", uiConfig);
+		}
+	}, []);
+	return (
+		<NoSSRComponent>
+			<div id="firebaseui-auth-container"></div>
+		</NoSSRComponent>
+	);
+}
