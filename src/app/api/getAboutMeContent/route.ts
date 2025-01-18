a80a6b6f4ed5921863f70838f805a42071c31750
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-18 04:16:40 -0500

    feat: simplify GET route by removing unused DynamoDB client initialization

--- src/app/api/getAboutMeContent/route.ts
+++ src/app/api/getAboutMeContent/route.ts
@@ -1,15 +1,1 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb"
-
-export function GET(request: Request) {
-    const dbClient = new DynamoDBClient({ region: "us-east-2" });
-    const docClient = DynamoDBDocumentClient.from(dbClient);
-
-    const params = {
-        TableName: "AboutMe",
-        Key: {
-            id: "about-me"
-        }
-    }
-
-  }
+export function GET(request: Request) {}
