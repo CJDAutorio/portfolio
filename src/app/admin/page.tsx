10b6144ce5108e85b603ce40a08ded2701fc25e6
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-18 04:15:10 -0500

    feat: implement dynamic view switching on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,28 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import React from "react";
+"use client";
+
+import React, { FormEvent } from "react";
+import { AboutMeForm } from "../Views/Admin/AboutMeForm/about-me-form.component";
 
 export default function Admin() {
-	const dbClient = new DynamoDBClient({});
-	const docClient = DynamoDBDocumentClient.from(dbClient);
-	
+	const [view, setView] = React.useState<React.ReactNode>(null);
+
+	const buttonClasses='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
+
+	function handleViewChange(component: React.ReactNode) {
+		setView(component);
+		console.log("set view to", component);
+	}
+
 	return (
-		<div>
-			<h1>Admin Page</h1>
+		<div className="container mx-auto mt-12 w-full">
+			<h1 className="text-4xl mb-16">Admin Page</h1>
+			<div className="flex flex-row w-full justify-around gap-x-2">
+				<button className={buttonClasses} onClick={() => handleViewChange(<AboutMeForm />)}>About Me Content</button>
+				<button className={buttonClasses} >Experience Content</button>
+				<button className={buttonClasses} >Hobbies Content</button>
+				<button className={buttonClasses} >Contact Me Content</button>
+			</div>
+			<div className="mt-12">{view}</div>
 		</div>
 	);
 }
