8493776ec4e0d249111bf396108342a485db08eb
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-19 16:27:12 -0500

    feat: add functions to set and delete documents in Firestore for aboutMe content management

--- src/app/Utils/FirestoreDB.ts
+++ src/app/Utils/FirestoreDB.ts
@@ -1,6 +1,40 @@
-import { getFirestore } from "firebase/firestore";
+import {
+	deleteDoc,
+	doc,
+	Firestore,
+	getFirestore,
+	setDoc,
+} from "firebase/firestore";
 import FirebaseApp from "./FirebaseApp";
+import { aboutMeContent } from "./types";
 
 const FirestoreDB = getFirestore(FirebaseApp);
+
+export async function setAboutMeDoc(
+	firestoreDb: Firestore,
+	content: aboutMeContent
+) {
+	const docRef = doc(firestoreDb, "aboutme-content");
+
+	await setDoc(docRef, content)
+		.then(() => {
+			console.log("Document written with ID: ", docRef.id);
+		})
+		.catch((error) => {
+			console.error("Error adding document: ", error);
+		});
+}
+
+export async function deleteDocument(firestoreDb: Firestore, docPath: string) {
+	const docRef = doc(firestoreDb, docPath);
+
+	await deleteDoc(docRef)
+		.then(() => {
+			console.log("Document deleted");
+		})
+		.catch((error) => {
+			console.error("Error deleting document: ", error);
+		});
+}
 
 export default FirestoreDB;
