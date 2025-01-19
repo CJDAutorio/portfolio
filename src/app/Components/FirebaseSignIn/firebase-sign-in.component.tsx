f8b2463c7cd2472acb2edb41464ed239cf8029f0
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-19 06:46:54 -0500

    feat: refactor Firebase sign-in component to use email/password authentication and improve UI

--- src/app/Components/FirebaseSignIn/firebase-sign-in.component.tsx
+++ src/app/Components/FirebaseSignIn/firebase-sign-in.component.tsx
@@ -1,6 +1,5 @@
 import FirebaseAuth from "@/app/Utils/FirebaseAuth";
-import firebase from "firebase/compat/app";
-import * as firebaseui from 'firebaseui'
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useEffect } from "react";
 import NoSSRComponent from "../NoSSR/no-s-s-r.component";
 
@@ -9,40 +8,52 @@ interface FirebaseSignInProps {
 }
 
 export function FirebaseSignIn({ setIsSignedIn }: FirebaseSignInProps) {
-	useEffect(() => {
+	function handleSubmit(event: React.FormEvent) {
+		event.preventDefault();
+		const email = (event.target as HTMLFormElement).email.value;
+		const password = (event.target as HTMLFormElement).password.value;
+		signInWithEmailAndPassword(FirebaseAuth, email, password).then((userCredential) => {
+			console.log('userCredential', userCredential);
+			setIsSignedIn(true);
+		});
+	}
 
-		const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(FirebaseAuth);
-
-		const uiConfig: firebaseui.auth.Config = {
-			signInOptions: [
-				{
-					provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-					requireDisplayName: false,
-					disableSignUp: {
-                        status: true
-                    }
-				},
-			],
-			signInFlow: "popup",
-			callbacks: {
-				signInSuccessWithAuthResult: function (authResult: unknown, _redirectUrl?: string) {
-					const currentUser = FirebaseAuth.currentUser;
-					console.log('currentUser', currentUser);
-					setIsSignedIn(true);
-					console.log("Sign in success", authResult);
-					return false;
-				},
-				signInFailure: function (error) {
-					console.error("Sign in failed", error);
-				},
-			},
-		};
-
-		ui.start("#firebaseui-auth-container", uiConfig);
-	}, []);
+	useEffect(() => {}, []);
 	return (
 		<NoSSRComponent>
-			<div id="firebaseui-auth-container"></div>
+			<div className="container mx-auto w-1/2 min-w-96">
+				<form
+					className="grid grid-cols-2 items-center justify-center gap-4"
+					onSubmit={handleSubmit}
+				>
+					<label htmlFor="email" className="place-self-end">
+						Email:
+					</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						required
+						className="border place-self-start"
+					/>
+					<label htmlFor="password" className="place-self-end">
+						Password:
+					</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						className="border place-self-start"
+						required
+					/>
+					<button
+						type="submit"
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-24 rounded col-span-2 mx-auto"
+					>
+						Sign In
+					</button>
+				</form>
+			</div>
 		</NoSSRComponent>
 	);
 }
