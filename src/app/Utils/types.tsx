c671b4686783fd9532316bad1c291aabd86e4bd3
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-19 06:47:05 -0500

    feat: add aboutMeContent interface to define structure for personal content with media support

--- src/app/Utils/types.tsx
+++ src/app/Utils/types.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThreeElements } from '@react-three/fiber';
+import { StorageReference } from "firebase/storage";
 
 export interface Button {
 	name: string;
@@ -8,4 +9,11 @@ export interface Button {
 
 declare module 'react' {
   type IntrinsicElements = ThreeElements
+}
+
+export interface aboutMeContent {
+	id: string;
+	content: string;
+	media: StorageReference | File | string;
+	title: string;
 }
