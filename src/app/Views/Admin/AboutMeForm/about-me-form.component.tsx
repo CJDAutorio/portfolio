b716f29e7466111c9281dc480ac5d85f71120bc4
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-19 20:39:06 -0500

    feat: refactor AboutMeForm to use centralized Firestore functions and improve state management

--- src/app/Views/Admin/AboutMeForm/about-me-form.component.tsx
+++ src/app/Views/Admin/AboutMeForm/about-me-form.component.tsx
@@ -2,32 +2,19 @@
 
 import { useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
-import { addDoc, Firestore } from "firebase/firestore";
-import { collection, getDocs } from "firebase/firestore";
 import { aboutMeContent } from "@/app/Utils/types";
 import { Accordion, AccordionItem } from "@szhsin/react-accordion";
 import Image from "next/image";
 import Modal from "react-modal";
-import {
-	ref,
-	uploadBytes,
-	getDownloadURL,
-	FirebaseStorage,
-} from "firebase/storage";
+import { getAllAboutMeDocs, uploadAboutMeDoc } from "@/app/Utils/FirestoreDB";
 
-interface AboutMeFormProps {
-	firestoreDb: Firestore;
-	firestoreStorage: FirebaseStorage;
-}
-
-export function AboutMeForm({
-	firestoreDb,
-	firestoreStorage,
-}: AboutMeFormProps) {
+export function AboutMeForm() {
 	const [aboutMeContent, setAboutMeContent] = useState<aboutMeContent[]>([]);
 	const [modalIsOpen, setModalIsOpen] = useState(false);
 	const [modalContent, setModalContent] = useState(<></>);
 	const [contentCode, setContentCode] = useState("<p></p>");
+	const [media, setMedia] = useState<File | null>(null);
+	const [isLoaded, setIsLoaded] = useState(false);
 
 	const modalStyles = {
 		content: {
@@ -40,165 +27,109 @@ export function AboutMeForm({
 		},
 	};
 
+	async function getAllAboutMeContent() {
+		setIsLoaded(false);
+		setAboutMeContent([]);
+		await getAllAboutMeDocs().then((content) => {
+			const aboutMeContent = content as aboutMeContent[];
+			console.log("content:", content);
+			setAboutMeContent(content);
+			console.log("about me content:", aboutMeContent);
+			setIsLoaded(true);
+		});
+	}
+
+	async function uploadAboutMeContent(content: aboutMeContent) {
+		await uploadAboutMeDoc(content).then(() => {
+			console.log("uploaded content", content);
+		});
+		setModalIsOpen(false);
+		getAllAboutMeContent();
+	}
+
+	const handleDelete = async (content: aboutMeContent) => {
+		console.log("deleting content", content);
+	};
+
 	Modal.setAppElement("#root");
 
-	function closeModal() {
-		setModalIsOpen(false);
-	}
-
-	async function getAboutMeContent() {
-		const querySnapshot = await getDocs(
-			collection(firestoreDb, "aboutme-content")
-		);
-		const contentPromises = querySnapshot.docs.map(async (doc) => {
-			const data = doc.data();
-			const downloadUrl = await getDownloadURL(
-				ref(
-					firestoreStorage as FirebaseStorage,
-					encodeURI(
-						`https://firebasestorage.googleapis.com/b/bucket/o/${data.media}`
-					)
-				)
-			);
-
-			return {
-				id: doc.id,
-				title: data.title,
-				content: data.content,
-				media: downloadUrl,
-			} as aboutMeContent;
-		});
-		const content = await Promise.all(contentPromises);
-		console.log(content);
-		setAboutMeContent(content);
-	}
-
-	async function addAboutMeContent(content: aboutMeContent) {
-		const file = content.media as File;
-
-		const storageRef = ref(firestoreStorage, `aboutme/${file.name}`);
-		uploadBytes(storageRef, content.media as File).then((snapshot) => {
-			console.log("Uploaded a blob or file!", snapshot);
-		});
-
-		const docRef = await addDoc(
-			collection(firestoreDb, "aboutme-content"),
-			{
-				title: content.title,
-				content: content.content,
-				media: `aboutme/${file.name}`,
-			}
-		);
-		console.log("Document written with ID: ", docRef.id);
-	}
-
-	function handleSubmit(event: React.FormEvent) {
-		event.preventDefault();
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 		setModalContent(
 			<div>
-				<h2>Are you sure you want to add the following entry?</h2>
-				<div>
+				<h1>Are you sure you want to submit the following content?</h1>
+				<div className="grid grid-cols-2 gap-4 border px-2 py-1 items-center">
+					<h2 className="font-bold">Section Title</h2>
 					<p>
-						<strong>Title:</strong>{" "}
-						{
-							(event.target as HTMLFormElement)["section-title"]
-								.value
-						}
+						{(e.target as HTMLFormElement)["section-title"].value}
 					</p>
-					<p>
-						<strong>Content:</strong> {contentCode}
+					<h2 className="font-bold">Section Content</h2>
+					<p className="text-sm sm:text-base bg-gray-800 text-white rounded-lg p-4 pl-6">
+						{contentCode}
 					</p>
-					<p>
-						<strong>Media:</strong>{" "}
-						{
-							(event.target as HTMLFormElement)["section-media"]
-								.value
-						}
-					</p>
+					<h2 className="font-bold">Section Media</h2>
+					{media ? (
+						<Image
+							src={URL.createObjectURL(media)}
+							alt="alt"
+							width={128}
+							height={128}
+						/>
+					) : (
+						<p>No media available</p>
+					)}
 				</div>
-				<div>
+				<div className="flex justify-center gap-4 mt-2">
 					<button
-						onClick={closeModal}
 						className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+						onClick={() => {
+							setModalIsOpen(false);
+						}}
 					>
-						No
+						Cancel
 					</button>
 					<button
+						className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
 						onClick={() => {
-							addAboutMeContent({
-								title: (event.target as HTMLFormElement)[
+							uploadAboutMeContent({
+								id: "",
+								title: (e.target as HTMLFormElement)[
 									"section-title"
 								].value,
 								content: contentCode,
-								media: (event.target as HTMLFormElement)[
-									"section-media"
-								].value,
-							} as aboutMeContent);
-							closeModal();
+								media: media ? media : "",
+							});
 						}}
-						className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
 					>
-						Yes
+						Submit
 					</button>
 				</div>
 			</div>
 		);
 		setModalIsOpen(true);
-		getAboutMeContent();
-	}
-
-	function handleDelete(aboutMeContent: aboutMeContent) {
-		setModalContent(
-			<div>
-				<h2>Are you sure you want to delete this entry?</h2>
-				<div>
-					<p>
-						<strong>Id:</strong> {aboutMeContent.id}
-					</p>
-					<p>
-						<strong>Title:</strong> {aboutMeContent.title}
-					</p>
-					<p>
-						<strong>Content:</strong> {aboutMeContent.content}
-					</p>
-					<p>
-						<strong>Media:</strong> {aboutMeContent.media}
-					</p>
-				</div>
-				<div>
-					<button
-						onClick={closeModal}
-						className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-					>
-						No
-					</button>
-					<button
-						onClick={() => {
-							// deleteAboutMeContent();
-							closeModal();
-						}}
-						className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-					>
-						Yes
-					</button>
-				</div>
-			</div>
-		);
-		setModalIsOpen(true);
-	}
+		console.log("submitting form");
+	};
 
 	useEffect(() => {
-		getAboutMeContent();
+		getAllAboutMeContent();
 	}, []);
 
 	return (
 		<div className="container mx-auto mt-12 w-full" id="about-me-form">
-			<div className="flex justify-center">
-				<h2 className="text-2xl mb-8">About Me Content Form</h2>
+			<div className="flex relative justify-center items-center w-full mb-8">
+				<h2 className="text-2xl">About Me Content Form</h2>
+				<button
+					onClick={getAllAboutMeContent}
+					className="absolute top-0 right-12 bg-slate-300 px-2 py-1 rounded"
+				>
+					Refresh
+				</button>
 			</div>
 			<Modal
 				isOpen={modalIsOpen}
-				onRequestClose={closeModal}
+				onRequestClose={() => {
+					setModalIsOpen(false);
+				}}
 				style={modalStyles}
 			>
 				{modalContent}
@@ -240,6 +171,9 @@ export function AboutMeForm({
 							name="section-media"
 							id="section-media"
 							className="border px-2 py-2"
+							onChange={(e) => {
+								setMedia(e.target.files?.[0] || null);
+							}}
 						/>
 						<button
 							type="submit"
@@ -254,49 +188,57 @@ export function AboutMeForm({
 			<div className="flex flex-col justify-center">
 				<h2 className="text-2xl my-8">Current About Me Content</h2>
 				<div className="border bg-slate-300">
-					{aboutMeContent ? (
-						aboutMeContent.map(
-							(content: aboutMeContent, index: number) => (
-								<div
-									key={index}
-									className="flex flex-col items-center justify-center px-4 py-2"
-								>
-									<h3 className="font-bold text-xl">
-										{content.title}
-									</h3>
-									<p>{content.id}</p>
-									<div className="grid grid-cols-12 w-full gap-4">
-										<p className="col-span-8 self-start">
-											{content.content}
-										</p>
-										<div className="col-span-4 self-end">
-											{content.media ? (
-												<Image
-													src={
-														content.media as string
-													}
-													alt={content.title}
-													width={48}
-													height={48}
-												/>
-											) : (
-												<p>No media available</p>
-											)}
+					{isLoaded ? (
+						<>
+							{aboutMeContent ? (
+								aboutMeContent.map(
+									(
+										content: aboutMeContent,
+										index: number
+									) => (
+										<div
+											key={index}
+											className="flex flex-col items-center justify-center px-4 py-2"
+										>
+											<h3 className="font-bold text-xl">
+												{content.title}
+											</h3>
+											<p>{content.id}</p>
+											<div className="grid grid-cols-12 w-full gap-4">
+												<p className="col-span-8 self-start">
+													{content.content}
+												</p>
+												<div className="col-span-4 self-end">
+													<Image
+														src={
+															content.media as string
+														}
+														alt={content.title}
+														width={96}
+														height={96}
+													/>
+													<p>{content.media as string}</p>
+												</div>
+											</div>
+											<button
+												onClick={() => {
+													handleDelete(content);
+												}}
+												className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4"
+											>
+												Delete
+											</button>
 										</div>
-									</div>
-									<button
-										onClick={() => {
-											handleDelete(content);
-										}}
-										className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4"
-									>
-										Delete
-									</button>
-								</div>
-							)
-						)
+									)
+								)
+							) : (
+								<p>No content available</p>
+							)}
+						</>
 					) : (
-						<p>No content available</p>
+						<>
+							<p>Loading...</p>
+						</>
 					)}
 				</div>
 			</div>
