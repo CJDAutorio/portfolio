3954b24e1beaa8932e66c3f134ce0da603d85bfb
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-21 11:26:43 -0500

    feat: implement CRUD operations for experience documents in Firestore

--- src/app/Utils/FirestoreDB.ts
+++ src/app/Utils/FirestoreDB.ts
@@ -9,7 +9,7 @@ import {
 	getFirestore,
 } from "firebase/firestore";
 import FirebaseApp from "./FirebaseApp";
-import { aboutMeContent } from './types';
+import { aboutMeContent, EducationExperience, ProjectExperience, WorkExperience } from './types';
 import { firebaseStorageGetDownloadURL, firebaseStorageUploadBytes } from "./FirebaseStorage";
 
 const FirestoreDB = getFirestore(FirebaseApp);
@@ -77,6 +77,68 @@ export async function getAllAboutMeDocs() {
     await Promise.all(promises);
 
     return content;
+}
+
+export async function uploadExperienceDoc(content: WorkExperience | ProjectExperience | EducationExperience) {
+    const docRef = collection(FirestoreDB, "experience-content");
+
+    const filePath = await firebaseStorageUploadBytes(content.media ? content.media as Blob : new Blob());
+
+    content.media = filePath;
+
+    await addDoc(docRef, content)
+        .then(() => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
+
+export async function getAllExperienceDocs() {
+    const querySnapshot = await getDocs(collection(FirestoreDB, "experience-content"));
+    const content: (WorkExperience | ProjectExperience | EducationExperience)[] = [];
+
+    const promises = querySnapshot.docs.map(async (doc) => {
+        const experienceContent = doc.data() as WorkExperience | ProjectExperience | EducationExperience;
+        console.log(`${doc.id}`, doc.data());
+        experienceContent.id = doc.id;
+        if (experienceContent.media) {
+            const url = await firebaseStorageGetDownloadURL(experienceContent.media as string);
+            console.log('url:', url);
+            experienceContent.media = url;
+        }
+        content.push(experienceContent);
+    });
+
+    await Promise.all(promises);
+
+    return content;
+}
+
+export async function getExperienceDoc(docId: string) {
+    const docRef = doc(FirestoreDB, `experience-content/${docId}`);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        return docSnap.data();
+    } else {
+        console.log("No such document!");
+        return null;
+    }
+}
+
+export async function deleteExperienceDoc(docId: string) {
+    const docRef = doc(FirestoreDB, `experience-content/${docId}`);
+
+    await deleteDoc(docRef)
+        .then(() => {
+            console.log("Document deleted");
+        })
+        .catch((error) => {
+            console.error("Error deleting document: ", error);
+        });
 }
 
 export default FirestoreDB;
