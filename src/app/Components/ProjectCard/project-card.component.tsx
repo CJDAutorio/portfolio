3dadcdd03b0d2e81df347edb039b109d944a3d60
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-22 14:56:27 -0500

    feat: add ProjectCard component to display project information

---
+++ src/app/Components/ProjectCard/project-card.component.tsx
@@ -0,0 +1,13 @@
+import { ProjectExperience } from "@/app/Utils/types";
+
+interface ProjectCardProps {
+	project: ProjectExperience;
+}
+
+export function ProjectCard({ project }: ProjectCardProps) {
+	return (
+		<>
+			<h1>ProjectCard</h1>
+		</>
+	);
+}
