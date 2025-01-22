a4dc4b2e3f1b002e3cedf4195f4099c539ecfcc1
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-01-22 14:56:25 -0500

    feat: update layout component to include font-sans class for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`antialiased ${geist.variable} ${sourceSerif4.variable}`}
+				className={`antialiased ${geist.variable} ${sourceSerif4.variable} font-sans`}
 				id="root"
 			>
 				<SpeedInsights />
