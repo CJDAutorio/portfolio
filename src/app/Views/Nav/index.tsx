3da2f76f1c5063302aa2571c85b2018a68c8a4f5
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-02-01 13:37:40 -0500

    feat: refactor Nav component to use classnames for dynamic class management

--- src/app/Views/Nav/index.tsx
+++ src/app/Views/Nav/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { HiOutlineChevronDown, HiOutlineChevronUp } from "react-icons/hi";
 import { Button } from "../../Utils/types";
 import "./style.css";
-import { motion } from "motion/react";
+import classNames from "classnames";
 
 interface NavComponentProps {
 	className?: string | undefined;
@@ -19,9 +19,6 @@ const Nav: React.FC<NavComponentProps> = ({
 }) => {
 	const [, setButtonHovered] = React.useState("");
 	const [buttonActivated, setButtonActivated] = React.useState(-1);
-
-	const buttonClasses =
-		"transition-all w-full px-12 py-1 hover:bg-slate-200/50 hover:drop-shadow-md hover:px-8 text-lg grid grid-cols-2 grid-rows-1 items-center";
 
 	const handleMouseIn = (e: React.MouseEvent<HTMLButtonElement>) => {
 		setButtonHovered(e.currentTarget.textContent || "");
@@ -44,38 +41,34 @@ const Nav: React.FC<NavComponentProps> = ({
 	};
 
 	return (
-		<motion.div
-			className={`${
-				className ? className : ""
-			} relative w-full h-full gap-4`}
-			animate={{
-				x: 0,
-				opacity: 1,
-				transition: {
-					default: { type: "spring" },
-					opacity: { ease: "linear" },
-				},
-			}}
+		<div
+			className={classNames(
+				className ? className : "",
+				`grid grid-cols-1 grid-rows-${buttons.length} gap-y-16`,
+			)}
 		>
-			{buttons.map((button, index) => {
-				return (
-					<button
-						key={index}
-						className={buttonClasses ? buttonClasses : ""}
-						onMouseEnter={handleMouseIn}
-						onMouseLeave={handleMouseOut}
-						onClick={() => handleMouseClick(index)}
-					>
-						<p className="justify-self-start font-sans">{button.name}</p>
+			{buttons.map((button, index) => (
+				<button
+					key={index}
+					className={classNames(
+						"w-full px-8 py-1 hover:bg-slate-100/50 border-b border-slate-100/50 hover:drop-shadow-md",
+						"text-lg grid grid-cols-2 grid-rows-1 items-center transition-colors"
+					)}
+					onMouseEnter={handleMouseIn}
+					onMouseLeave={handleMouseOut}
+					onClick={() => handleMouseClick(index)}
+				>
+					<p className="justify-self-start">{button.name}</p>
+					<div className="justify-self-end">
 						{buttonActivated === index ? (
-                            <div className="justify-self-end"><HiOutlineChevronUp /></div>
+							<HiOutlineChevronUp fontSize={24} />
 						) : (
-							<div className="justify-self-end"><HiOutlineChevronDown /></div>
+							<HiOutlineChevronDown fontSize={24} />
 						)}
-					</button>
-				);
-			})}
-		</motion.div>
+					</div>
+				</button>
+			))}
+		</div>
 	);
 };
 
