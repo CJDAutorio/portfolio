63b1ea849f9876c879c2e708c4dac55eeaab3544
CJ D'Autorio <cj.dautorio2021@gmail.com>
2025-02-02 20:06:01 -0500

    feat: update layout in Home component for improved positioning and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ import { motion } from "motion/react";
 export default function Home() {
 	return (
 		<div className="overflow-hidden relative h-screen w-screen">
-			<div className="z-10 absolute top-4 md:top-12 md:right-40 flex flex-col justify-center items-start md:justify-end md:items-end gap-y-2 md:gap-y-4">
-				<h1 className="font-serif font-semibold text-right text-4xl md:text-6xl tracking-wider leading-none">
+			<div className="z-10 fixed top-0 md:top-0 md:right-20 flex flex-col justify-center items-start md:justify-end md:items-end gap-y-2 md:gap-y-4">
+				<h1 className="font-serif font-semibold text-right text-2xl md:text-5xl tracking-wider leading-none">
 					CJ D&apos;Autorio
 				</h1>
-				<h2 className="font-serif font-normal text-right text-2xl md:text-4xl tracking-wide leading-none">
+				<h2 className="font-serif font-normal text-right text-xl md:text-2xl tracking-wide leading-none">
 					Developer
 				</h2>
 				<div className="flex justify-end items-end gap-4">
@@ -23,15 +23,15 @@ export default function Home() {
 					</a>
 				</div>
 			</div>
+			<div className="z-0 pointer-events-none fixed overflow-hidden h-screen w-screen m-0 p-0">
+				<ThreeJSScene />
+			</div>
 			<div className="z-10 relative">
 				<div
-					className="absolute"
+					className=""
 				>
 					<Base />
 				</div>
-			</div>
-			<div className="z-0 pointer-events-none absolute overflow-hidden h-screen w-screen m-0 p-0">
-				<ThreeJSScene />
 			</div>
 		</div>
 	);
